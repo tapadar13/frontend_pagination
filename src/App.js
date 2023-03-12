aee46b1cb80dc9123076cec299e3697ca8944efa
tapadar13 <monsurismyname@gmail.com>
2023-03-12 18:57:33 +0530

    pagination implemented

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import axios from "axios";
 function App() {
   const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchProducts = async () => {
     try {
@@ -25,18 +26,39 @@ function App() {
     fetchProducts();
   }, []);
 
+  const selectPageHandler = (selectedPage) => {
+    setPage(selectedPage);
+  };
+
   return (
     <div>
       {products.length > 0 && (
         <div className="products">
-          {products.map((prod) => {
+          {products.slice(page * 6 - 6, page * 6).map((prod) => {
             return (
-              <span className="products__list" key={prod.id}>
-                <img src={prod.thumbnail} alt={prod.title} />
-                <span>{prod.title}</span>
+              <span className="products__list" key={prod?.id}>
+                <img src={prod?.thumbnail} alt={prod?.title} />
+                <span>{prod?.title}</span>
               </span>
             );
           })}
+        </div>
+      )}
+      {products.length > 0 && (
+        <div className="pagination">
+          <span>◀️</span>
+          {[...Array(products.length / 6)].map((_, idx) => {
+            return (
+              <span
+                className={page === idx + 1 ? "pagination__selected" : ""}
+                onClick={() => selectPageHandler(idx + 1)}
+                key={idx}
+              >
+                {idx + 1}
+              </span>
+            );
+          })}
+          <span>▶️</span>
         </div>
       )}
     </div>
